d8ade769db6f5d04510b9d4f7a72da1549670509
laikaDe <ldeunert@gmail.com>
2022-07-18 18:02:16 -0700

    1. Defined prop reauirements in TaskList
    2. Created TaskList test states in the story file
    
    3. import Task.js to TaskList and pass attributes and actions

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
 import Task from './Task';
 
+//Here the Task object is stored as a default object with the props
+//component: Task, and title: 'Task'
 export default {
   component: Task,
   title: 'Task',
 };
 
 const Template = args => <Task {...args} />;
+//Jsx elements are capitalized 
+//Template takes in all properties of the Task component with ...notation syntax
 
+// Template.bind({}) is assigned to Default 
+// This stores the default args of task to Default object
+// so that the Default object can be called again in different 
+// functions such as Pinned and Archived
 export const Default = Template.bind({});
 Default.args = {
   task: {
